d34187705d60418239f6279d08705c6f530e3a12
jahun koo <koo10682@gmail.com>
2020-01-08 15:31:37 +0900

    chore: setup

---
+++ packages/tokens/src/typo.js
@@ -0,0 +1,136 @@
+const baseTypo = { fontFamily: 'Radikal' }
+
+export default {
+    h: {
+        1: {
+            ...baseTypo,
+            fontWeight: 300,
+            fontSize: '72px',
+            lineHeight: '81px',
+            letterSpacing: '-2px',
+        },
+        2: {
+            ...baseTypo,
+            fontWeight: 300,
+            fontSize: 42,
+            lineHeight: '54px',
+            letterSpacing: '-1.5px',
+        },
+        3: {
+            ...baseTypo,
+            fontWeight: 700,
+            fontSize: 42,
+            lineHeight: '44px',
+            letterSpacing: 0,
+        },
+        4: {
+            ...baseTypo,
+            fontWeight: 700,
+            fontSize: 28,
+            lineHeight: '32px',
+            letterSpacing: 0,
+        },
+        5: {
+            ...baseTypo,
+            fontWeight: 500,
+            fontSize: 26,
+            lineHeight: '32px',
+            letterSpacing: 0,
+        },
+        6: {
+            ...baseTypo,
+            fontWeight: 400,
+            fontSize: 20,
+            lineHeight: '28px',
+            letterSpacing: 0,
+        },
+        7: {
+            ...baseTypo,
+            fontWeight: 400,
+            fontSize: 18,
+            lineHeight: '26px',
+            letterSpacing: 0,
+        },
+
+    },
+    heading: {
+        ...baseTypo,
+        fontWeight: 500,
+        fontSize: 16,
+        lineHeight: '24px',
+        letterSpacing: 0,
+    },
+    subheading: {
+        ...baseTypo,
+        fontWeight: 500,
+        fontSize: 13,
+        lineHeight: '16px',
+        letterSpacing: 0.5,
+        textTransform: 'uppercase',
+    },
+    button: {
+        ...baseTypo,
+        fontWeight: 400,
+        fontSize: 14,
+        lineHeight: '20px',
+        letterSpacing: 0.4,
+    },
+    buttonLarge: {
+        ...baseTypo,
+        fontWeight: 300,
+        fontSize: 20,
+        lineHeight: '28px',
+        letterSpacing: 0.6,
+    },
+    label: {
+        ...baseTypo,
+        fontWeight: 400,
+        fontSize: 14,
+        lineHeight: '20px',
+        letterSpacing: 0.4,
+    },
+    caption: {
+        ...baseTypo,
+        fontWeight: 400,
+        fontSize: 12,
+        lineHeight: '16px',
+        letterSpacing: 0.2,
+    },
+    body: {
+        1: {
+            ...baseTypo,
+            fontWeight: 300,
+            fontSize: 28,
+            lineHeight: '32px',
+            letterSpacing: 0,
+        },
+        2: {
+            ...baseTypo,
+            fontWeight: 300,
+            fontSize: 18,
+            lineHeight: '26px',
+            letterSpacing: 0,
+        },
+        3: {
+            ...baseTypo,
+            fontWeight: 300,
+            fontSize: 16,
+            lineHeight: '24px',
+            letterSpacing: 0,
+        },
+        4: {
+            ...baseTypo,
+            fontWeight: 300,
+            fontSize: 14,
+            lineHeight: '20px',
+            letterSpacing: 0,
+        },
+        5: {
+            ...baseTypo,
+            fontWeight: 300,
+            fontSize: 12,
+            lineHeight: '16px',
+            letterSpacing: 0,
+        },
+    }
+}
