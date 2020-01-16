9afb82969279da19eccd9af29e07ccc819504eb5
jahun koo <koo10682@gmail.com>
2020-01-16 18:12:47 +0900

    style: update prettier config

--- packages/button/src/index.tsx
+++ packages/button/src/index.tsx
@@ -2,7 +2,13 @@ import { colors } from '@sds/theme';
 import styled from 'styled-components';
 
 const Button = styled.button`
-    background: ${props => (props.color ? 'red' : colors.red[400])};
+    background: ${(props) => {
+        console.log(
+            111111111111111111111111111111111111111111111111111111,
+            1111111111111111111111111111,
+        );
+        return props.color ? 'red' : colors.red[400];
+    }};
     color: #fff;
     border-radius: 3px;
     cursor: pointer;
