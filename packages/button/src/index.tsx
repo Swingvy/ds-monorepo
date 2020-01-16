111abc45161fde84ce8f62ab72a466963ab2c02f
jahun koo <koo10682@gmail.com>
2020-01-16 18:54:20 +0900

    chore(button): remove teset console

--- packages/button/src/index.tsx
+++ packages/button/src/index.tsx
@@ -3,10 +3,6 @@ import styled from 'styled-components';
 
 const Button = styled.button`
     background: ${(props) => {
-        console.log(
-            111111111111111111111111111111111111111111111111111111,
-            1111111111111111111111111111,
-        );
         return props.color ? 'red' : colors.red[400];
     }};
     color: #fff;
